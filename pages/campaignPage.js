88c7f77f5251e49a09fe11c3b53a2fc0bcc4e73d
jincyraju123 <152210251+jincyraju123@users.noreply.github.com>
2025-07-22 21:39:13 -0400

    Campaign Module code  added

---
+++ pages/campaignPage.js
@@ -0,0 +1,61 @@
+ export class campaignPage{
+
+    constructor(page) {
+      this.page = page;
+      this.username = '#username';
+      this.password = '#inputPassword';
+      this.loginButton = 'button[type="submit"]';
+      this.campaignTab = 'a[href="/campaigns"]';
+      this.createCampaign='button[class="btn btn-info"]';
+      this.campaignNameField='[name="campaignName"]';
+      this.campaignStatusField='[name="campaignStatus"]';
+      this.targetSizeField='[name="targetSize"]';
+      this.expectedCloseDateField='[name="expectedCloseDate"]';
+      this.targetAudienceField='[name="targetAudience"]';
+      this.descriptionField='textarea[name="description"]';
+      this.createCampaignButton='button[type="submit"]';
+
+
+    }
+  
+ async goto(url) {
+      await this.page.goto(url);
+      await this.page.waitForLoadState('networkidle');
+      return await this.page.url();
+    }
+  
+async login(username, password) {
+      
+    await this.page.fill(this.username, username);
+    await this.page.fill(this.password, password);
+    await this.page.click(this.loginButton);
+
+}
+
+async clickCampaignsTab(){
+
+    await this.page.click(this.campaignTab);
+}
+
+async clickCreateCampaign(){
+    await this.page.click(this.createCampaign)
+}
+async createCampaignWithAllFields(data){
+
+    await this.page.fill(this.campaignNameField,data.name);
+    await this.page.fill(this.campaignStatusField,data.status);
+    await this.page.fill(this.targetSizeField,data.targetSize);
+    await this.page.fill(this.expectedCloseDateField,data.expectedCloseDate);
+    await this.page.fill(this.targetAudienceField,data.targetAudience);
+    await this.page.fill(this.descriptionField,data.description);
+
+}
+async clickCreateCampaignButton(){
+
+    await this.page.click(this.createCampaignButton);
+}
+
+
+}
+
+//module.exports = { campaignPage };
---
+++ testdata/campaignData.js
@@ -0,0 +1,22 @@
+export const testData = {
+
+
+  URLs: {
+    url: 'http://49.249.28.218:8098',
+    successLoginUrl:'http://49.249.28.218:8098/dashboard',
+    successCampaignCreationURL:'http://49.249.28.218:8098/campaigns',
+  },
+  login: {
+    username: 'rmgyantra',
+    password: 'rmgy@9999',
+  },
+  campaign: {
+    name: 'Product Launch',
+    status: 'Active',
+    targetSize: '1000',
+    expectedCloseDate: '2025-12-28',
+    targetAudience: 'customers',
+    description: 'This campaign targeting  customers with special offers',
+    
+  }
+};
